a6221a6266034e7e55540115ff073464a709a5e2
linweiyuan <root@linweiyuan.com>
2023-05-05 19:07:28 +0800

    docs(linweiyuan): update chatgpt-proxy-server

--- linweiyuan/chatgpt-proxy-server/run.sh
+++ linweiyuan/chatgpt-proxy-server/run.sh
@@ -1,7 +1,7 @@
 set -m
 
 if [ -z "$LOG_LEVEL" ]; then
-    export LOG_LEVEL=INFO
+    export LOG_LEVEL=OFF
 fi
 
 ./undetected_chromedriver --allowed-ips= --allowed-origins=* --log-level="$LOG_LEVEL"
