29e752413c7be0bfc2e5bf3b75d8e21f71755145
linweiyuan <root@linweiyuan.com>
2023-05-13 21:18:38 +0800

    docs(linweiyuan): update chatgpt-proxy-server-warp

--- linweiyuan/chatgpt-proxy-server-warp/run.sh
+++ linweiyuan/chatgpt-proxy-server-warp/run.sh
@@ -5,7 +5,9 @@ if [ -z "$LOG_LEVEL" ]; then
 fi
 
 warp-svc | grep "$LOG_LEVEL" &
+
 sleep 1
+
 warp-cli --accept-tos set-mode proxy
 
 if [ -z "$TEAMS_ENROLL_TOKEN" ]; then
@@ -15,8 +17,15 @@ else
     warp-cli --accept-tos teams-enroll-token $TEAMS_ENROLL_TOKEN
 fi
 
-warp-cli --accept-tos enable-always-on
+socat TCP-LISTEN:65535,fork TCP:127.0.0.1:40000 &
 
-socat TCP-LISTEN:65535,fork TCP:127.0.0.1:40000
+while true; do
+    if [[ $(warp-cli --accept-tos warp-stats | awk 'NR==3') == *GB ]]; then
+        warp-cli --accept-tos delete
+        warp-cli --accept-tos register
+        warp-cli --accept-tos set-mode proxy
+        warp-cli --accept-tos connect
+    fi
 
-fg %1
+    sleep 300
+done
