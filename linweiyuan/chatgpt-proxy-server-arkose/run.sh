da416ee2374b456eec1cab88c2d5778f5e5e6100
linweiyuan <root@linweiyuan.com>
2023-07-16 15:25:59 +0800

    docs(linweiyuan): update chatgpt-proxy-server-arkose

--- linweiyuan/chatgpt-proxy-server-arkose/run.sh
+++ linweiyuan/chatgpt-proxy-server-arkose/run.sh
@@ -1,9 +1,1 @@
-set -m
-
-if [ -z "$LOG_LEVEL" ]; then
-    export LOG_LEVEL=fatal
-fi
-
-python -m http.server &
-
-geckodriver --host 0.0.0.0 --allow-hosts chatgpt-proxy-server-arkose --log "$LOG_LEVEL"
+cd /chatgpt-arkose-token-api && ./chatgpt-arkose-token-api
